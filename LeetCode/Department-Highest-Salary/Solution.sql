10bcd423e062f07cce2a5f45cb067efa735b4a08
Vladimir <71398553+Vo-one@users.noreply.github.com>
2026-03-05 14:47:08 +0300

    Add solution for Department Highest Salary

--- LeetCode/Department-Highest-Salary/Solution.sql
+++ LeetCode/Department-Highest-Salary/Solution.sql
@@ -1,5 +1,5 @@
 1# Write your MySQL query statement below
-2select Department.name as Department, e.name as Employee, e.salary as Salary 
+2select d.name as Department, e.name as Employee, e.salary as Salary 
 3from Employee e
 4join (
 5    select departmentId, max(salary) as salary
@@ -8,5 +8,5 @@
 8) t
 9on e.departmentId = t.departmentId 
 10and e.salary = t.salary
-11join Department 
-12on Department.id = e.departmentId 
+11join Department d
+12on d.id = e.departmentId 
