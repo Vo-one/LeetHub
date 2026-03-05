8c01d64a11de27512de335994e2e96b7371e8d90
Vladimir <71398553+Vo-one@users.noreply.github.com>
2026-03-05 15:24:11 +0300

    Add SQL query to find top three salaries by department

--- LeetCode/Department-Top-Three-Salaries/Solution.sql
+++ LeetCode/Department-Top-Three-Salaries/Solution.sql
@@ -1,1 +1,9 @@
-Department 
+# Write your MySQL query statement below
+select d.name as Department, t.name as Employee, salary as Salary
+from (
+    select *,
+    DENSE_RANK() OVER (partition by departmentId ORDER BY salary DESC) AS RankBySalary 
+    from Employee e
+) t
+join Department d on t.departmentId = d.id
+where t.RankBySalary <= 3
