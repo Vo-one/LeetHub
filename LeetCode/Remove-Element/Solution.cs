16217ceea6f9efe7c081d9a648eccc048613b428
Vladimir <71398553+Vo-one@users.noreply.github.com>
2026-03-06 11:49:17 +0300

    Add solution for Remove Element

--- LeetCode/Remove-Element/Solution.cs
+++ LeetCode/Remove-Element/Solution.cs
@@ -1,14 +1,8 @@
 1public class Solution {
-2    public int RemoveElement(int[] nums, int val) {
-3    int k = 0;
-4
-5     for (int i = 0; i < nums.Length; i++) 
-6     {
-7        if(nums[i] != val){
-8            nums[k] = nums[i]; 
-9            k++;
-10        }
-11     }
-12    return k;
-13    }
-14}
+2    public int RemoveElement(int[] nums, int val)
+3    {
+4        int k = 0;
+5        foreach (var x in nums) if (x != val) nums[k++] = x;
+6        return k;
+7    }
+8}
